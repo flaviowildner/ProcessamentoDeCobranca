46540b0e017f2d5b98e3b7e0e1c1d253078333ee
flaviowildner <flaviowildner@hotmail.com>
2020-11-22 01:43:53 -0300

    Fixes a bug where the mongodb filter throws an error when the last month of the year is queried

--- CobrancaAPI/Persistence/Repositories/MongoDB/FilterStrategies/MesMongoFilterDefinitionStrategy.cs
+++ CobrancaAPI/Persistence/Repositories/MongoDB/FilterStrategies/MesMongoFilterDefinitionStrategy.cs
@@ -10,8 +10,10 @@ namespace CobrancaAPI.Persistence.Repositories.MongoDB.FilterStrategies
         public FilterDefinition<Cobranca> FilterDefinition(string value)
         {
             DateTime dateValue = DateTime.ParseExact(value, "yyyy-MM", CultureInfo.InvariantCulture);
+
             DateTime startDate = new DateTime(dateValue.Year, dateValue.Month, 1);
-            DateTime endDate = new DateTime(dateValue.Year, dateValue.Month + 1, 1);
+            DateTime endDate = startDate.AddMonths(1).AddSeconds(-1);
+
             return Builders<Cobranca>.Filter.Gte(cobranca => cobranca.Vencimento, startDate) &
                    Builders<Cobranca>.Filter.Lt(cobranca => cobranca.Vencimento, endDate);
         }
